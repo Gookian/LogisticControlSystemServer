e7c6936218b32c0c3fa21c656f3e21661347de69
Daniil <48077576+Gookian@users.noreply.github.com>
2024-04-07 21:29:48 +0700

    Добавьте файлы проекта.

---
+++ LogisticControlSystemServer/Startup.cs
@@ -0,0 +1,41 @@
+﻿namespace LogisticControlSystemServer
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSignalR();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1",
+                new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Swagger Demo API",
+                    Description = "Demo API for showing Swagger",
+                    Version = "v1"
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger Demo API");
+                options.RoutePrefix = "";
+            });
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "api/{controller=Order}/{action=GetAll}/{id?}");
+            });
+        }
+    }
+}
